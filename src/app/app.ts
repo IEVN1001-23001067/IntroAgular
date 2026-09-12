e17500d1e0c10fbb82014aaf631122ee13f749ec
IEVN1001-23001067 <83191@alumnosutleon.edu.mx>
2026-09-12 09:59:07 -0600

    Heroes 12-09-26

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,5 +7,15 @@ import { Component, signal } from '@angular/core';
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('IntroAngular');
+  title = 'Marisol';
+
+  duplicaNumero(n1:number):number{
+    return n1 * 2
+  }
+
+  pelicula={
+    titulo: 'Spider Man',
+    fechaLanzamiento: new Date(),
+    precio:1234
+  }
 }
