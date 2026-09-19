eced7c1ff5b69673ba90724de2c24384fa8476de
IEVN1001-23001067 <83191@alumnosutleon.edu.mx>
2026-09-19 10:05:50 -0600

    Pipes Filtros 19-09-26

--- src/app/heroes/heroes-list/heroes-list.ts
+++ src/app/heroes/heroes-list/heroes-list.ts
@@ -1,8 +1,53 @@
 import { Component } from '@angular/core';
+import { IHeroes } from '../heroes';
 
 @Component({
   selector: 'app-heroes-list',
   standalone: false,
   templateUrl: './heroes-list.html',
 })
-export class HeroesList {}
+export class HeroesList {
+  imageWidth:number=40;
+  imageMargin:number=2;
+  muestraImage:boolean=true;
+  listFilter:String='';
+
+  showImage():void{
+    this.muestraImage=!this.muestraImage;
+  }
+
+
+  heroes: IHeroes[]=[
+    {
+    imagen:'https://dragonball-api.com/characters/goku_normal.webp',
+    nombre:'Goku',
+    description:'Kame Hame Ha',
+    race:'Saiyan',
+    ki:9000
+  },
+
+   {
+    imagen:'https://dragonball-api.com/characters/vegeta_normal.webp',
+    nombre:'Vegeta',
+    description:'Principe Saiyan',
+    race:'Saiyan',
+    ki:8000
+  },
+
+   {
+    imagen:'https://dragonball-api.com/characters/picolo_normal.webp',
+    nombre:'Piccolo',
+    description:'Verde',
+    race:'Namek',
+    ki:6000
+  },
+
+   {
+    imagen:'https://dragonball-api.com/characters/bulma.webp',
+    nombre:'Bulma',
+    description:'Mujer',
+    race:'Humano',
+    ki:0
+  }
+  ]
+}
---
+++ src/app/heroes/heroes.ts
@@ -0,0 +1,9 @@
+export interface IHeroes {
+
+    imagen:string;
+    nombre:string;
+    description:string;
+    race:string;
+    ki:number;
+
+}
